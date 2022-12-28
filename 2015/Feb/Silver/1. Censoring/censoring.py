f815e8c52f5c4932fd51804e9894f6b3e81748fa
Shen <shen+github@wan.camp>
2022-12-28 10:35:17 -0500

    Use Python's replace() method

--- 2015/Feb/Silver/1. Censoring/censoring.py
+++ 2015/Feb/Silver/1. Censoring/censoring.py
@@ -3,5 +3,16 @@ http://www.usaco.org/index.php?page=viewproblem2&cpid=529
 '''
 
 S = input()
+M = len(S)
+
 T = input()
 N = len(T)
+
+while True:
+    S = S.replace(T, "", 1)
+    new_len = len(S)
+    if new_len == M:
+        break
+    M = new_len
+
+print(S)
