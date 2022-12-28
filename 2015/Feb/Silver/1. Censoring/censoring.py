ea51fe9235b18500b8d71781dd9ad24538346525
Shen <shen+github@wan.camp>
2022-12-28 15:22:16 -0500

    File I/O

--- 2015/Feb/Silver/1. Censoring/censoring.py
+++ 2015/Feb/Silver/1. Censoring/censoring.py
@@ -2,6 +2,24 @@
 http://www.usaco.org/index.php?page=viewproblem2&cpid=529
 '''
 
-S = input()
-T = input()
+# S = input()
+# T = input()
+
+
+def read_line_from_file(f):
+    s = input_file.readline()
+    return s.rstrip("\n")
+
+
+with open("censor.in", "r") as input_file:
+    S = read_line_from_file(input_file)
+    T = read_line_from_file(input_file)
+
+M = len(S)
 N = len(T)
+
+# TODO: implementation
+
+# print(S)
+with open("censor.out", "w") as output_file:
+    output_file.write(S)
